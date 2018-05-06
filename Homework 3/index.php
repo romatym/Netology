e28356ce649cdeec39d0c1254805ebe3f648dc26
Roma <Roma@Roma>
2018-05-07 01:43:23 +0300

    Merge branch 'master' of https://github.com/romatym/Netology

--- Homework 3/index.php
+++ Homework 3/index.php
@@ -6,8 +6,8 @@
     </head>
     <body>
         <?php
-//            $continents = ['Europe', 'Asia', 'Africa', 'North America', 'South America', 'Australia', 'Antarctica'];
-            $animals = [
+            
+        $animals = [
                 'Europe' => ['Canis lupus', 'Vulpes vulpes', 'Alces alces'],
                 'Africa' => ['Loxodonta africana', 'Panthera leo', 'Pan troglodytes'],
                 'Australia' => ['Macropus'],
@@ -16,15 +16,35 @@
                 'North America' => ['Bison bison'],
                 'South America' => ['Myrmecophagidae']
                 ];
-            echo '<pre>';
-            print_r($animals);
             
+            $two_words = [];
+            $first_words = [];
+            $second_words = [];
             foreach ($animals as $continent => $animal) {
-                echo '<b>' . $continent, '</b><br>';
                 foreach ($animal as $key => $name) {
-                    echo $key, $name . '<br>';
+                    $pos = strpos($name, ' ');
+                    if ($pos !== FALSE) {
+                        
+                        $two_words[] = $name;
+                        $pieces = explode(" ", $name);
+                        $first_words[] = $pieces[0];
+                        $second_words[] = $pieces[1];
+                    } 
                 }
             }
+            
+            shuffle($first_words);
+            shuffle($second_words);
+            
+            $new_animals = [];
+            
+            foreach ($first_words as $key1 => $first_word) {
+                $new_animals[] = $first_words[$key1].' '.$second_words[$key1];
+            }
+            
+            echo '<pre>';
+            print_r($new_animals);
+            
         ?>
     </body>
 </html>
