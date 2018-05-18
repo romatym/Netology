7a172be7528bbd4bcb522e6f4433926d138c6443
Roma <Roma@Roma>
2018-05-19 01:21:49 +0300

    Merge branch 'master' of https://github.com/romatym/Netology

---
+++ Homework6/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+    
+    <form action="action.php" method="post">
+        <p>Ваше имя: <input type="text" name="name" /></p>
+        <p>Ваш возраст: <input type="text" name="age" /></p>
+        <p><input type="submit" /></p>
+    </form>
+<?php       
+?>
+        
+    </body>
+</html>
