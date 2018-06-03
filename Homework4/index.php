08cd730b1da18f9c9db4209bd06a42630ca14675
Roma <Roma@Roma>
2018-06-03 17:52:13 +0300

    Merge branch 'master' of https://github.com/romatym/Netology

--- Homework4/index.php
+++ Homework4/index.php
@@ -1,3 +1,30 @@
+<?php
+    $parameters = '?id=5601538&appid=ef275086da6b4c2a604c04dd29f2e5dc';
+    $link = 'https://api.openweathermap.org/data/2.5/weather';
+    $content = file_get_contents($link.$parameters);
+    if ($content === false) {
+        exit('Не удалось получить данные');
+    }
+    $content_decoded = json_decode($content);
+    if ($content_decoded === false) {
+        exit('Не удалось преобразовать данные');
+    }
+    $temperature = round($content_decoded->main->temp - 273.15); //Перевод из Кельвинов в градусы Цельсия
+    $pressure = round($content_decoded->main->pressure);
+    $humidity = round($content_decoded->main->humidity);
+
+    echo '<pre>';
+    echo 'Погода в Москве:';
+    $weather_description = $content_decoded->weather;
+    if (isset($weather_description) & count($weather_description) > 0) {
+        echo '<br/>';
+        echo($weather_description[0]->description);
+    }
+    echo('<br/>Температура: ' . (!empty($temperature) ? $temperature, : 'Нет данных');
+    echo('<br/>Давление: ' . $pressure);
+    echo('<br/>Влажность: ' . $humidity);
+    echo('</pre>';
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,24 +32,6 @@
         <title></title>
     </head>
     <body>
-        <?php
-            $content = file_get_contents("https://api.openweathermap.org/data/2.5/weather?id=5601538&appid=ef275086da6b4c2a604c04dd29f2e5dc");
-            $content_decoded = json_decode($content);
-            $temperature = $content_decoded->main->temp - 273.15; //Перевод из Кельвинов в градусы Цельсия
-            $pressure = $content_decoded->main->pressure;
-            $humidity = $content_decoded->main->humidity;
-
-            echo '<pre>';
-            echo 'Погода в Москве:';
-            $weather_description = $content_decoded->weather;
-            if (isset($weather_description) & count($weather_description)>0) {
-                echo '<br/>';
-                echo($weather_description[0]->description);
-            }
-            echo '<br/>Температура: '.round($temperature);
-            echo '<br/>Давление: '.round($pressure);
-            echo '<br/>Влажность: '.round($humidity);
-            echo '</pre>';
-        ?>
+        
     </body>
 </html>
