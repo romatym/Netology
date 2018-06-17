ccbaf96882d4e8a5b237b7443924306da95abbd2
Roma <Roma@Roma>
2018-06-17 22:21:24 +0300

    Merge branch 'master' of https://github.com/romatym/Netology

---
+++ Homework3/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            
+        $animals = [
+                'Europe' => ['Canis lupus', 'Vulpes vulpes', 'Alces alces'],
+                'Africa' => ['Loxodonta africana', 'Panthera leo', 'Pan troglodytes'],
+                'Australia' => ['Macropus'],
+                'Asia' => ['Ailuropoda melanoleuca'],
+                'Antarctica' => ['Spheniscidae'],
+                'North America' => ['Bison bison'],
+                'South America' => ['Myrmecophagidae']
+                ];
+            
+            echo '<pre>';
+            echo 'Исходные животные:';
+            print_r($animals);
+            
+            $two_words = [];
+            $first_words = [];
+            $second_words = [];
+            
+            foreach ($animals as $continent => $animal) 
+            {
+                foreach ($animal as $key => $name) 
+                {
+                    $pos = explode(" ", $name);
+                    if (count($pos) == 2) 
+                    {
+                        $two_words[] = $name;
+                        $pieces = explode(" ", $name);
+                        $first_words[] = $pieces[0];
+                        $second_words[] = $pieces[1];
+                    } 
+                }
+            }
+            
+            shuffle($first_words);
+            shuffle($second_words);
+            
+            $new_animals = [];
+            
+            foreach ($first_words as $key1 => $first_word) 
+            {
+                $new_animals[] = $first_words[$key1].' '.$second_words[$key1];
+            }
+            
+            echo '<pre>';
+            echo '<br>Новые животные:';
+            print_r($new_animals);
+          
+            $continent_new_animals = [];
+            
+            foreach ($animals as $continent => $animals_array) 
+            {
+                $new_animals2 = [];
+                
+                foreach ($animals_array as $key1 => $animal) 
+                {
+                    foreach ($new_animals as $key2 => $new_animal) 
+                    {
+                        $pieces = explode(" ", $new_animal);
+                        $find_animal = stristr($animal, $pieces[0]);
+                        
+                        if ($find_animal != false) 
+                        {
+                            $new_animals2[] = $new_animal;
+                        }
+                    }                
+                }
+                
+                $continent_new_animals[$continent] = $new_animals2;
+            }
+
+            echo '<br>Новые животные по континентам:';
+            foreach ($continent_new_animals as $continent => $animals_array) 
+            {
+                echo '<h2>'.$continent.'</h2>';
+                echo implode(", ", $animals_array);
+            }
+            
+        ?>
+    </body>
+</html>
