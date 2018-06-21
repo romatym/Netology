42637ff0f2dce26421c0ff549c168a60a0e37ecc
Roma <Roma@Roma>
2018-06-21 19:59:18 +0300

    Merge branch 'master' of https://github.com/romatym/Netology

---
+++ Homework15/index.php
@@ -0,0 +1,46 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+$api = new \Yandex\Geo\Api();
+
+// Можно искать по точке
+$api->setPoint(30.5166187, 50.4452705);
+
+// Или можно икать по адресу
+$api->setQuery('Тверская 6');
+
+// Настройка фильтров
+$api
+    ->setLimit(1) // кол-во результатов
+    ->setLang(\Yandex\Geo\Api::LANG_US) // локаль ответа
+    ->load();
+
+$response = $api->getResponse();
+$response->getFoundCount(); // кол-во найденных адресов
+$response->getQuery(); // исходный запрос
+$response->getLatitude(); // широта для исходного запроса
+$response->getLongitude(); // долгота для исходного запроса
+
+// Список найденных точек
+$collection = $response->getList();
+foreach ($collection as $item) {
+    $item->getAddress(); // вернет адрес
+    $item->getLatitude(); // широта
+    $item->getLongitude(); // долгота
+    $item->getData(); // необработанные данные
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
